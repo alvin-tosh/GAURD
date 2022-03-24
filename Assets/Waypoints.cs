608fddb4edd50767032a6ca3eb0997c571777648
alvin-tosh <alvingitonga14@gmail.com>
2022-03-24 15:48:45 +0300

    initial commits

---
+++ Assets/Waypoints.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+            
+        }
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
